54aef94331a7a2573c579ebe02fe4367215b89d1
AliNedjar <alinedjarali45@outlook.com>
2020-11-04 17:04:42 +0100

    finished US_21

---
+++ src/Model/HomeManager.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class HomeManager extends AbstractManager
+{
+    /**
+     *
+     */
+    public const TABLE = 'bike';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    public function selectNewestBikes()
+    {
+        return $this->pdo->query('SELECT name, image FROM ' . self::TABLE . ' ORDER BY created_date DESC LIMIT 4 ')->fetchAll();
+
+    }
+}
