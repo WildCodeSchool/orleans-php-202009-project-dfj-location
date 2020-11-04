56ce0366db26d1a8bd9d48775aea94e35ebe7e74
Flichyn <nicolasflichy@gmail.com>
2020-11-04 16:26:09 +0100

    Finished user story

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+use App\Model\CategoryManager;
+
+class CategoryController extends AbstractController
+{
+
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $categoryManager = new CategoryManager();
+        $categories = $categoryManager->selectAll();
+        return $this->twig->render('Category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+}
