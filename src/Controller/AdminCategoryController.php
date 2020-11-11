2f86f5d64330442b2bd9d315233b73ec543bb24b
Flichyn <nicolasflichy@gmail.com>
2020-11-11 18:02:46 +0100

    Finished user story

---
+++ src/Controller/AdminCategoryController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\CategoryManager;
+
+class AdminCategoryController extends AbstractController
+{
+
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function add()
+    {
+        $categoryManager = new CategoryManager();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $category = array_map('trim', $_POST);
+            $errors = [];
+            if (empty($category['name'])) {
+                $errors[] = 'Vous devez choisir un nom pour cette catégorie.';
+            }
+            $length = 100;
+            if (strlen($category['name']) > $length) {
+                $errors = 'Le nom de la catégorie ne doit pas dépasser ' . $length . '.';
+            }
+            if (empty($errors)) {
+                $categoryManager->insert($category);
+                header('Location: /AdminCategory/index');
+            }
+        }
+
+        return $this->twig->render('Admin/addCategory.html.twig', [
+            'errors' => $errors ?? [],
+            'category' => $category ?? [],
+        ]);
+    }
+}
