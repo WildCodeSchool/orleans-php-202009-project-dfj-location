bcda0288ab34db09e36b475a6ec3a7868bc70219
bloic <loic.benos@gmail.com>
2020-11-16 18:55:40 +0100

    udapte files

--- src/Controller/AdminBicycleController.php
+++ src/Controller/AdminBicycleController.php
@@ -3,22 +3,11 @@
 namespace App\Controller;
 
 use App\Model\AdminBicycleManager;
-use App\model\BicycleManager;
 use App\Model\CategoryManager;
 
 class AdminBicycleController extends AbstractController
 {
     public const TABLE = 'bike';
-
-    protected const TABLE1 = 'reservation';
-
-    public function index()
-    {
-        $adminBikeManager = new BicycleManager();
-        $bikes = $adminBikeManager->selectAllWithCategories();
-
-        return $this->twig->render('Admin/bikes.html.twig', ['bikes' => $bikes]);
-    }
 
     public function editorBike()
     {
