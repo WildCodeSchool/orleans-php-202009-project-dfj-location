cb3bca300d8c1b7b770c8eb5a470730c82453881
bloic <loic.benos@gmail.com>
2020-11-16 18:54:44 +0100

    udapte files

--- src/Controller/AdminBicycleController.php
+++ src/Controller/AdminBicycleController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use App\Model\BicycleManager;
-use App\Model\ReservationManager;
+use App\Model\AdminBicycleManager;
+use App\model\BicycleManager;
+use App\Model\CategoryManager;
 
 class AdminBicycleController extends AbstractController
 {
@@ -17,5 +18,61 @@ class AdminBicycleController extends AbstractController
         $bikes = $adminBikeManager->selectAllWithCategories();
 
         return $this->twig->render('Admin/bikes.html.twig', ['bikes' => $bikes]);
+    }
+
+    public function editorBike()
+    {
+        $categoryManager = new CategoryManager();
+        $categories = $categoryManager->selectAll();
+        $bike = [];
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $bike = array_map('trim', $_POST);
+            $errors = $this->validateBike($bike);
+
+            if (empty($errors)) {
+                $adminBicycleManager = new AdminBicycleManager();
+                $adminBicycleManager->update($bike);
+                header("location:/AdminBicycle/index");
+            }
+        }
+        return $this->twig->render('Admin/editor-bike.html.twig', ['errors' => $errors ?? [],
+            'bike' => $bike, 'categories' => $categories
+
+        ]);
+    }
+    /**
+     * @SuppressWarnings(PHPMD)
+     * @param array $bike
+     * @return array
+     */
+
+    private function validateBike(array $bike)
+    {
+        $errors = [];
+        if (empty($bike['name'])) {
+            $errors[] = 'Le nom du vélo est obligatoire.';
+        }
+        $length = 100;
+        if (strlen($bike['name']) > $length) {
+            $errors[] = 'Le nom du vélo ne doit pas dépasser ' . $length . ' caractères.';
+        }
+
+        if (empty($bike['weight']) || $bike['weight'] < 0) {
+            $errors[] = 'Le poids du vélo est obligatoire et doit être positif.';
+        }
+
+        if (empty($bike['stock']) || $bike['stock'] < 0) {
+            $errors[] = 'le nombre de vélo ne peut être inférieur à 0';
+        }
+
+        if (!empty($bike['category_id'] === 'catégorie')) {
+            $errors[] = 'La selection de la catégorie est obligatoire.';
+        }
+
+        if (empty($bike['image']) || !filter_var($bike['image'], FILTER_VALIDATE_URL)) {
+            $errors[] = 'L\'image doit être une URL valide et le champs ne doit pas être vide.';
+        }
+
+        return $errors;
     }
 }
---
+++ src/Model/AdminBicycleManager.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Model;
+
+/**
+ *
+ */
+class AdminBicycleManager extends AbstractManager
+{
+    public const TABLE = 'bike';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    public function update(array $bike): bool
+    {
+
+        // prepared request
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET 'name' = :name, weight = :weight,
+        category_id = :category_id, image = :image, maximum_charge = :maximum_charge, autonomy = :autonomy,
+        frame_size = :frame_size, stock = :stock WHERE id=:id");
+        $statement->bindValue('id', $bike['id'], \PDO::PARAM_INT);
+        $statement->bindValue('name', $bike['name'], \PDO::PARAM_STR);
+        $statement->bindValue('weight', $bike['weight']);
+        $statement->bindValue('category_id', $bike['category_id'], \PDO::PARAM_INT);
+        $statement->bindValue('image', $bike['image'], \PDO::PARAM_STR);
+        $statement->bindValue('maximum_charge', $bike['maximum_charge'], \PDO::PARAM_INT);
+        $statement->bindValue('autonomy', $bike['autonomy'], \PDO::PARAM_STR);
+        $statement->bindValue('frame_size', $bike['frame_size'], \PDO::PARAM_STR);
+        $statement->bindValue('stock', $bike['stock'], \PDO::PARAM_INT);
+
+        return $statement->execute();
+    }
+}
